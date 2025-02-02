f569ce8ade0d2ee19ab136978248223a17f8f1a2
Luis Torres <tluistorres@example.com>
2025-02-02 16:35:29 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+const amigosOriginais = ['Livia', 'Louise', 'Lucas', 'Quiteria', 'Luis']; 
+const amigos = []; // Começa com a lista vazia
+const eAmigoInput = document.querySelector("#amigo");
+const eBotaoAdicionar = document.getElementById('adicionarNome');
+const eAmigoList = document.querySelector("#listaNomes");
+const eBotaoSortear = document.getElementById('sortearAmigo');
+const eAmigoResultado = document.querySelector("#resultado");
+const eBotaoLimpar = document.querySelector("#limparLista");
+
+eBotaoLimpar.onclick = limparLista; 
+eBotaoSortear.addEventListener('click', sortearAmigo);
+eBotaoAdicionar.addEventListener('click', adicionarAmigo);
+
+function adicionarAmigo() {
+  const amigo = eAmigoInput.value.trim();
+
+  const mensagemErro = validarAmigo(amigo);
+  if (mensagemErro) {
+    mostrarMensagemErro(mensagemErro);
+    return;
+  }
+  amigos.push(amigo); // Adiciona o amigo ao array
+  atualizarListaAmigos(); // Atualiza a lista na interface
+  eAmigoInput.value = ""; // Limpa o campo de entrada
+}
+
+function validarAmigo(amigo) {
+  if (amigo.length < 3) {
+    return "O nome do amigo deve ter pelo menos 3 caracteres";
+  }
+
+  if (!amigosOriginais.includes(amigo)) {
+    return "O nome deve estar na lista original de amigos";
+  }
+
+  if (amigos.includes(amigo)) {
+    return "Amigo já adicionado";
+  }
+
+  return null; // Retorna null se não houver erro
+}
+
+function mostrarMensagemErro(mensagem) {
+  window.alert(mensagem); // Você pode substituir por um elemento na interface
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    window.alert("Adicione ao menos um amigo antes de sortear.");
+    return;
+  }
+  const randIndex = Math.floor(Math.random() * amigos.length);
+  const resultadoAmigo = amigos[randIndex]; // Seleciona um amigo aleatório
+  const resultadoMessage = `<span style="color: #05DF05; font-weight: bold;">Parabéns! O sorteado foi ${resultadoAmigo}.</span>`; // Mensagem de resultado
+  
+  eAmigoResultado.innerHTML = resultadoMessage; // Exibe o resultado na tela com HTML
+}
+// Funções auxiliares
+function limparLista() {
+  amigos.length = 0; // Limpa o array de amigos
+  atualizarListaAmigos(); // Atualiza a interface para refletir a lista vazia
+  eAmigoResultado.textContent = ""; // Limpa o resultado exibido
+}
+
+function atualizarListaAmigos() {
+  eAmigoList.innerHTML = ""; // Limpa a lista exibida
+  amigos.forEach((amigo) => {
+    const li = document.createElement("li");
+    li.textContent = amigo; // Adiciona o nome do amigo à lista
+    eAmigoList.appendChild(li); // Adiciona o item à lista no HTML
+  });
+}
